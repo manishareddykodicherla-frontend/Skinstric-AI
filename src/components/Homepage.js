ec196ce5e763c22c5c3b146d685c15637beb297c
manishareddykodicherla-frontend <manishareddykodicherla@gmail.com>
2026-06-30 00:19:13 -0700

    made it styles accoring to screen size

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import {useState} from 'react';
 import Styles from"../index.css"
 import buttinIcon from "../assets/buttin-icon-shrunk.png";
 import right from "../assets/right.png"
-import left from "../assets/left.png"
+import left from "../assets/Rectangle 2778 (2).png"
+
 export default function Homepage() {
+  const [isDiscoverHover, setIsDiscoverHover]= useState(false);
+  const[isTakeTestHover, setIsTakeTestHover]= useState(false);
   return (
     <div>
         <header>
@@ -13,18 +17,29 @@ export default function Homepage() {
     <button className="enter__code">Entercode</button>
 </header>
 <div className="rectangle">
+  <div className="diamond"> </div>
+  <div className="diamond__inner"></div>
   <img src={right} alt="dotted" className="dottedline"/>
   <button className="button__shrunk"> 
     <img src={buttinIcon} alt=""></img>
     <span>DiscoverAI</span>
     </button>
-<h1> Sophisticated skincare</h1>
+<h1 className="mainTitle"> Sophisticated <br/>skincare</h1>
 <img src={left} alt="dotted" className='dottedline2'/>
 <button className="takeTest">
   <img src={buttinIcon} alt=""/>
   <span>Taketest</span>
 </button>
-    </div> 
+
+
+<h2 className="description">Skinstric developed an A.I. that creates a
+highly-personalized routine tailored to
+what your skin needs.
+
+</h2>
+<button  className="enterExperience">ENTER EXPERIENCE</button>
+</div>
+    
     </div>
   )
 }
